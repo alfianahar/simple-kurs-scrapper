f991eb89e5ad99ab72acc687ef9f70f36df058c0
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2023-06-03 03:01:13 +0700

    update : finishing indexing

--- routes/indexing.js
+++ routes/indexing.js
@@ -7,87 +7,97 @@ const sqlConnection = require('../config/dbConfig');
 const getIndexing = async (req, res) => {
     const url = 'https://www.bca.co.id/id/informasi/kurs';
 
-    axios.get(url)
-        .then(response => {
-            const $ = cheerio.load(response.data);
-            const tableRows = $('.m-table-kurs tbody tr');
+    const currentDate = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
 
-            const data = [];
+    try {
+        // Check if data for the current date already exists in ExchangeRate table
+        const checkDateQuery = 'SELECT COUNT(*) AS count FROM ExchangeRate WHERE createdDate = ?';
+        const [checkDateResult] = await sqlConnection.query(checkDateQuery, [currentDate]);
 
-            tableRows.each(async (index, element) => {
-                const currencyCode = $(element).attr('code');
-                const eRateBuy = parseFloat($(element).find('[rate-type="ERate-buy"]').text().replace('.', '').replace(',', '.'));
-                const eRateSell = parseFloat($(element).find('[rate-type="ERate-sell"]').text().replace('.', '').replace(',', '.'));
-                const ttBuy = parseFloat($(element).find('[rate-type="TT-buy"]').text().replace('.', '').replace(',', '.'));
-                const ttSell = parseFloat($(element).find('[rate-type="TT-sell"]').text().replace('.', '').replace(',', '.'));
-                const bnBuy = parseFloat($(element).find('[rate-type="BN-buy"]').text().replace('.', '').replace(',', '.'));
-                const bnSell = parseFloat($(element).find('[rate-type="BN-sell"]').text().replace('.', '').replace(',', '.'));
-                const date = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+        if (checkDateResult[0].count > 0) {
+            return res.status(200).json({ message: 'Data for the current date already exists.' });
+        }
 
-                const formattedData = {
-                    symbol: currencyCode,
-                    e_rate: {
-                        jual: eRateSell,
-                        beli: eRateBuy
-                    },
-                    tt_counter: {
-                        jual: ttSell,
-                        beli: ttBuy
-                    },
-                    bank_notes: {
-                        jual: bnSell,
-                        beli: bnBuy
-                    },
-                    date: date
-                };
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        const tableRows = $('.m-table-kurs tbody tr');
 
-                data.push(formattedData);
+        const data = [];
 
-                try {
-                    // Check if symbol already exists in Currency table
-                    const checkQuery = 'SELECT COUNT(*) AS count FROM Currency WHERE symbol = ?';
-                    const [checkResult] = await sqlConnection.query(checkQuery, [currencyCode]);
+        for (let i = 0; i < tableRows.length; i++) {
+            const currencyCode = $(tableRows[i]).attr('code');
+            const eRateBuy = parseFloat($(tableRows[i]).find('[rate-type="ERate-buy"]').text().replace('.', '').replace(',', '.'));
+            const eRateSell = parseFloat($(tableRows[i]).find('[rate-type="ERate-sell"]').text().replace('.', '').replace(',', '.'));
+            const ttBuy = parseFloat($(tableRows[i]).find('[rate-type="TT-buy"]').text().replace('.', '').replace(',', '.'));
+            const ttSell = parseFloat($(tableRows[i]).find('[rate-type="TT-sell"]').text().replace('.', '').replace(',', '.'));
+            const bnBuy = parseFloat($(tableRows[i]).find('[rate-type="BN-buy"]').text().replace('.', '').replace(',', '.'));
+            const bnSell = parseFloat($(tableRows[i]).find('[rate-type="BN-sell"]').text().replace('.', '').replace(',', '.'));
 
-                    if (checkResult.length > 0) {
-                        const currencyId = checkResult[0].currency_id;
-                        const exchangeRateId = uuidv4();
-                        const eRateId = uuidv4();
-                        const ttCounterId = uuidv4();
-                        const bankNotesId = uuidv4();
+            const formattedData = {
+                symbol: currencyCode,
+                e_rate: {
+                    jual: eRateSell,
+                    beli: eRateBuy
+                },
+                tt_counter: {
+                    jual: ttSell,
+                    beli: ttBuy
+                },
+                bank_notes: {
+                    jual: bnSell,
+                    beli: bnBuy
+                },
+                date: currentDate
+            };
 
-                        // Insert data into ERate table
-                        const eRateQuery = 'INSERT INTO ERate (erate_id, jual, beli) VALUES (?, ?, ?)';
-                        await sqlConnection.query(eRateQuery, [eRateId, eRateSell, eRateBuy]);
+            data.push(formattedData);
+        }
 
-                        // Insert data into TTCounter table
-                        const ttCounterQuery = 'INSERT INTO TTCounter (tt_counter_id, jual, beli) VALUES (?, ?, ?)';
-                        await sqlConnection.query(ttCounterQuery, [ttCounterId, ttSell, ttBuy]);
+        for (const item of data) {
+            try {
+                const currencyCode = item.symbol;
 
-                        // Insert data into BankNotes table
-                        const bankNotesQuery = 'INSERT INTO BankNotes (bank_notes_id, jual, beli) VALUES (?, ?, ?)';
-                        await sqlConnection.query(bankNotesQuery, [bankNotesId, bnSell, bnBuy]);
+                // Check if symbol already exists in Currency table (case-insensitive comparison)
+                const checkQuery = 'SELECT currency_id FROM Currency WHERE LOWER(symbol) = LOWER(?)';
+                const [checkResult] = await sqlConnection.query(checkQuery, [currencyCode.toLowerCase()]);
 
-                        // Insert data into ExchangeRate table
-                        const exchangeRateQuery = 'INSERT INTO ExchangeRate (exchange_rate_id, currency_id, erate_id, tt_counter_id, bank_notes_id, createdDate) VALUES (?, ?, ?, ?, ?, ?)';
-                        await sqlConnection.query(exchangeRateQuery, [exchangeRateId, currencyId, eRateId, ttCounterId, bankNotesId, date]);
+                if (checkResult.length > 0) {
+                    const currencyId = checkResult[0].currency_id;
+                    const exchangeRateId = uuidv4();
+                    const eRateId = uuidv4();
+                    const ttCounterId = uuidv4();
+                    const bankNotesId = uuidv4();
 
-                    } else {
-                        // Symbol doesn't exist in Currency table, handle the case accordingly
-                        console.log(`Symbol ${currencyCode} does not exist in Currency table.`);
-                    }
+                    // Insert data into ERate table
+                    const eRateQuery = 'INSERT INTO ERate (erate_id, jual, beli) VALUES (?, ?, ?)';
+                    await sqlConnection.query(eRateQuery, [eRateId, item.e_rate.jual, item.e_rate.beli]);
 
-                } catch (error) {
-                    console.error(error);
-                    res.status(500).send('An error occurred while inserting data into the database.');
+                    // Insert data into TTCounter table
+                    const ttCounterQuery = 'INSERT INTO TTCounter (tt_counter_id, jual, beli) VALUES (?, ?, ?)';
+                    await sqlConnection.query(ttCounterQuery, [ttCounterId, item.tt_counter.jual, item.tt_counter.beli]);
+
+                    // Insert data into BankNotes table
+                    const bankNotesQuery = 'INSERT INTO BankNotes (bank_notes_id, jual, beli) VALUES (?, ?, ?)';
+                    await sqlConnection.query(bankNotesQuery, [bankNotesId, item.bank_notes.jual, item.bank_notes.beli]);
+
+                    // Insert data into ExchangeRate table
+                    const exchangeRateQuery = 'INSERT INTO ExchangeRate (exchange_rate_id, currency_id, erate_id, tt_counter_id, bank_notes_id, createdDate) VALUES (?, ?, ?, ?, ?, ?)';
+                    await sqlConnection.query(exchangeRateQuery, [exchangeRateId, currencyId, eRateId, ttCounterId, bankNotesId, currentDate]);
+
+                } else {
+                    // Symbol doesn't exist in Currency table, handle the case accordingly
+                    console.log(`Symbol ${currencyCode} does not exist in Currency table.`);
                 }
-            });
-
-            res.json(data);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send('An error occurred while scraping the data.');
-        });
+            } catch (error) {
+                console.error(error);
+                return res.status(500).send('An error occurred while inserting data into the database.');
+            }
+        }
+        return res.json(data);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('An error occurred while scraping the data.');
+    }
 };
 
 module.exports = {
